a95dfdd72019b08939779b6c997a2faf74969476
Holegots <fuergaosi@gmail.com>
2020-02-26 00:03:05 +0800

    [feature] 增加提示

--- serverless.js
+++ serverless.js
@@ -120,13 +120,14 @@ class AuthingOidc extends Component {
         inputs.fromClientRemark || "Authing-OIDC";
       const tencentApiGatewayOutputs = await tencentApiGateway(apigwParam);
       outputs.route = endpoints
+
       outputs.apiGatewayServiceId = tencentApiGatewayOutputs.serviceId;
       outputs.url = `${this.getDefaultProtocol(
         tencentApiGatewayOutputs.protocols
       )}://${tencentApiGatewayOutputs.subDomain}/${
         tencentApiGatewayOutputs.environment
       }/`;
-
+      outputs.info = `请到 Auting 控制台将 ${outputs.url}/code2token/ 添加至 回调 URL`
       if (tencentApiGatewayOutputs.customDomains) {
         outputs.customDomains = tencentApiGatewayOutputs.customDomains;
       }
